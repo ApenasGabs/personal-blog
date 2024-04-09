7917f1b41c911977a049b470b6e43f9f67f41569
Gabriel Rodrigues <gabers357@gmail.com>
2024-04-09 07:57:06 -0300

    Add Home component to App.tsx and create Home.tsx

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="container">
+      <h1>Home</h1>
+      <img src="https://github.com/Apenasgabs.png" alt="" />
+    </div>
+  );
+};
+
+export default Home;
