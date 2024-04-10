568877835757da9b9004ead06b282e34cacca6da
Gabriel Rodrigues <gabers357@gmail.com>
2024-04-10 15:06:50 -0300

    Add DaisyUI plugin to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
+/* global require */
 /** @type {import('tailwindcss').Config} */
 export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"];
 export const theme = {
   extend: {},
 };
-export const plugins = [];
+export const plugins = [require("daisyui")];
