2ea6f3ce9e26b6a3d9d48a9a8b56f9198d70655b
Gabriel Rodrigues <gabers357@gmail.com>
2024-04-10 11:49:05 -0300

    Update Home component to include a counter and click functionality

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="container">
-      <h1>Home</h1>
-      <img src="https://github.com/Apenasgabs.png" alt="" />
+      <h1 className="">Home</h1>
+      <p>voce clicou {counter}x</p>
+      <button onClick={() => setCounter((prev) => prev + 1)}>Click me </button>
     </div>
   );
 };
