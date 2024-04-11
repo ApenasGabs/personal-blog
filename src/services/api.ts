778ee776e1ab4f456919875132230e4893ee9456
Gabriel Rodrigues <gabers357@gmail.com>
2024-04-11 17:07:30 -0300

    Add axios dependency to package.json and yarn.lock

---
+++ src/services/api.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const baseURl = "";
+const api = axios.create({
+  baseURL: baseURl,
+});
+interface RequestPros {
+  url: string;
+  data: object;
+  header?: object;
+  setData: (data: object) => void;
+}
+export const registerUser = async ({ url, data, setData }: RequestPros) => {
+  const response = await api.post(url, data);
+  setData(response.data);
+};
+
+export const login = async ({ url, data, setData }: RequestPros) => {
+  const response = await api.post(url, data);
+  setData(response.data);
+};
+
+export const fetch = async ({ url, header, setData }: RequestPros) => {
+  const response = await api.get(url, header);
+  setData(response.data);
+};
+
+export const register = async ({ url, header, data, setData }: RequestPros) => {
+  const response = await api.post(url, data, header);
+  setData(response.data);
+};
+
+export const update = async ({ url, header, data, setData }: RequestPros) => {
+  const response = await api.put(url, data, header);
+  setData(response.data);
+};
+
+export const deleteUser = async (url: string, header: object) => {
+  await api.delete(url, header);
+};
