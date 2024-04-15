673b51bb89e8c2242208f14d03950ab2f3fd43cc
Gabriel Rodrigues <gabers357@gmail.com>
2024-04-15 15:44:22 -0300

    Update CSS styles and login page layout

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   const [counter, setCounter] = useState(0);
   return (
     <div className="flex flex-col items-center">
-      <h1>Home</h1>
+      <h1>Bem vindo</h1>
       <img className="w-1/2" src="https://github.com/Apenasgabs.png" alt="" />
       <p>voce clicou {counter}x</p>
       <button onClick={() => setCounter((prev) => prev + 1)}>Click me </button>
