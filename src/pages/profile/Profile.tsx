46075eac62dedf45b27578bd9ff5aace6f1e1460
Gabriel Rodrigues <gabers357@gmail.com>
2024-04-21 19:07:25 -0300

    Corrigidos estilos do componente Navbar e adicionado link para o perfil do usuário no menu

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,35 +1,15 @@
-import {
-  ChangeEvent,
-  useCallback,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
-import { useNavigate } from "react-router-dom";
+import { ChangeEvent, useCallback, useContext, useState } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 import { UserProfile } from "../../models/UserProfile";
 import { update } from "../../services/api";
+import useGitHubUserProfile from "../../hooks/useGitHubUserProfile";
 
 const Profile = () => {
-  const navigate = useNavigate();
   const [userCredentials, setUserCredentials] = useState<UserProfile>(
     {} as UserProfile
   );
-  const [passwordsMatch, setPasswordsMatch] = useState(true);
-
+  const { githubUserProfileImage, hasProfileImage } = useGitHubUserProfile();
   const { isLoading, user, setUser } = useContext(AuthContext);
-
-  useEffect(() => {
-    if (user.token !== "") {
-      navigate("/home");
-    }
-  }, [navigate, user]);
-
-  useEffect(() => {
-    setPasswordsMatch(
-      userCredentials.password === userCredentials.confirmPassword
-    );
-  }, [userCredentials.password, userCredentials.confirmPassword]);
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setUserCredentials({
@@ -38,7 +18,7 @@ const Profile = () => {
     });
   };
 
-  const handleSubmitSignUp = useCallback(
+  const handleSubmitUpdate = useCallback(
     async (e: ChangeEvent<HTMLFormElement>) => {
       e.preventDefault();
       update({
@@ -49,14 +29,15 @@ const Profile = () => {
     },
     [setUser, user.id, userCredentials]
   );
-  const githubUserProfileImage = `https://github.com/${user.photo}.png`;
 
   return (
     <div className="flex justify-center items-center w-screen flex-col mt-12">
-      <img src={githubUserProfileImage} alt="github User Profile Image" />
+      {hasProfileImage && (
+        <img src={githubUserProfileImage} alt="github User Profile Image" />
+      )}
       <form
         className="flex justify-center items-center flex-col w-1/2 gap-4 p-10"
-        onSubmit={handleSubmitSignUp}
+        onSubmit={handleSubmitUpdate}
       >
         <label className="input input-bordered flex items-center gap-2">
           <svg
@@ -73,7 +54,7 @@ const Profile = () => {
             type="text"
             id="photo"
             name="photo"
-            value={userCredentials.photo}
+            value={user.photo}
             className="grow"
             placeholder="GitHub Username"
           />
@@ -92,7 +73,7 @@ const Profile = () => {
             type="text"
             id="name"
             name="name"
-            value={userCredentials.name}
+            value={user.name}
             className="grow"
             placeholder="Name"
           />
@@ -112,40 +93,41 @@ const Profile = () => {
             type="text"
             id="email"
             name="email"
-            value={userCredentials.email}
+            readOnly
+            value={user.email}
             className="grow"
             placeholder="Email"
           />
         </label>
-
-        <button
-          className="btn-active btn-primary"
-          type="submit"
-          disabled={!passwordsMatch}
-        >
-          {isLoading ? (
-            <div role="status">
-              <svg
-                aria-hidden="true"
-                className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                viewBox="0 0 100 101"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                  fill="currentColor"
-                />
-                <path
-                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                  fill="currentFill"
-                />
-              </svg>
-            </div>
-          ) : (
-            <p>Save</p>
-          )}
-        </button>
+        <div className="just">
+          <button className="btn-active btn-primary" type="submit">
+            {isLoading ? (
+              <div role="status">
+                <svg
+                  aria-hidden="true"
+                  className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+              </div>
+            ) : (
+              <p>Save</p>
+            )}
+          </button>
+          <button className="btn-active btn-secondary" type="submit">
+            Cancel
+          </button>
+        </div>
       </form>
     </div>
   );
